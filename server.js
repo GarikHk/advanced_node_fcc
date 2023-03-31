a07f3833119684652c72e259b2290266891220ac
Garik H <garik.hakobyan.2004@gmail.com>
2023-03-31 21:15:55 +0300

    feat: password hashing added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
 const session = require('express-session');
 const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
+//const res = require('express/lib/response');
+const bcrypt = require('bcrypt');
 const app = express();
 
 app.set('view engine', 'pug');
@@ -26,17 +29,114 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Connect to the database and serialize/deserialize the user
-myDB( async client => {
-  const myDataBase = await client.db('database').collection('users');
-  
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+
+myDB(async client => {
+  const myDataBase = await client.db('MyDataBase').collection('users');
   console.log('Connected to the database');
+  const users = await myDataBase.find().toArray();
+  console.log(users);
+
+  //Routes
+  //Default Route
   app.route('/').get((req, res) => {
+    console.log('Get request for index.pug')
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please log in',
+      showLogin: true,
+      showRegistration: true,
     });
   });
+
+  //Login Route
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    console.log('Post request for login')
+    res.redirect('/profile');
+  });
+
+  //Profile Route
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    console.log('Get request for profile.pug')
+    res.render('profile', {
+      username: req.user.username,
+    });
+  });
+
+  //Logout Route
+  app.route('/logout').get((req, res, next) => {
+    req.logout((err) => {
+      if (err) return next(err)
+    });
+    res.redirect('/');
+    console.log('User logges out!')
+  });
+  
+  //Register Route
+  app.route('/register')
+    .post((req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          const hash = bcrypt.hashSync(req.body.password, 12);
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: hash,
+          },
+            (err, doc) => {
+              if (err) {
+                res.redirect('/');
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+          console.log("User created!");
+        }
+      });
+    },
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res, next) => {
+        res.redirect('/profile');
+      }
+    );
+
+  //Not Found
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+    console.log("User Not Found!")
+  });
+
+  //Create a strategy and serialize 
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        console.log(user)
+        if (err) return done(err);
+        if (!user) {
+          console.log('User not found')
+          return done(null, false)
+        };
+        const isValid = bcrypt.compareSync(password, user.password)
+        if (!isValid) return done(null, false);
+        console.log('User is logged in!')
+        return done(null, user);
+      });
+    })
+  );
 
   passport.serializeUser((user, done) => {
     console.log('User serialized');
@@ -45,11 +145,12 @@ myDB( async client => {
 
   passport.deserializeUser((id, done) => {
     console.log('User deserialized');
-    myDataBase.findById(id, (err, doc) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
     });
   });
-}).catch( e => {
+
+}).catch(e => {
   console.log('Uable to connect to the database');
   app.route('/').get((req, res) => {
     res.render('index', {
